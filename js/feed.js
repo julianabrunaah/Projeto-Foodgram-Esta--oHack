4695d8aa67294efa591b383d719b34db2067a705
julianabrunaah <julianabrunahh@gmail.com>
2021-01-07 19:50:21 -0300

    Create feed.js

---
+++ js/feed.js
@@ -0,0 +1,9 @@
+//capturar nome
+const displayNome = document.querySelector(".avatar-content b");
+console.log(displayNome);
+
+//capturar informação do  localStoreg
+const nome = localStorage.getItem('nomeDoVisitante');
+console.log(nome);
+
+//escrever o nome
